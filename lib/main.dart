57a432837b03ea48c48f7f5846ac36cb7ef1b3eb
Sagnik Biswas <me@sagnik.dev>
2022-10-18 19:20:14 +0530

    :sparkles: Change Name

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Session One'),
     );
   }
 }
